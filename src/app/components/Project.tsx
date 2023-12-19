62d52b4b38e77090129e65ec972b369624ae8c42
DonovinNatividad <donovincall@gmail.com>
2023-12-19 15:22:04 -0500

    Added projects section and smoothed out lots of UI settings

---
+++ src/app/components/Project.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Image from 'next/image';
+
+interface ProjectProps {
+    title: string;
+    description: string;
+    technologies: string[];
+    image: string;
+    githubLink: string;
+}
+
+const Project: React.FC<ProjectProps> = ({ title, description, technologies, image, githubLink }) => {
+    return (
+        <a href={githubLink} target="_blank" rel="noopener noreferrer" className=' lg:w-3/4 p-4 transform hover:scale-105 transition-transform duration-200'>
+            <div className='flex flex-col items-start bg-gray-900 rounded-xl p-4 h-full border-b border-black'>
+                <Image src={image} alt={title} className='w-full object-cover rounded-xl' height={378} width={512} />
+                <h2 className='mt-4 text-2xl font-bold text-rose-300'>{title}</h2>
+                <p className='mt-2 text-white flex-grow'>{description}</p>
+                <p className='mt-2 text-sm text-gray-300'>{technologies.join(', ')}</p>
+            </div>
+        </a>
+    );
+}
+
+export default Project;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import LanguageCard from './components/LanguageCard';
 import Badge from './components/Badge';
 import Job from './components/Job';
+import Project from './components/Project';
 
 const Page: React.FC = () => {
   return (
@@ -31,8 +32,8 @@ const Page: React.FC = () => {
 
           <div className='flex flex-col sm:flex-row items-center'>
             <li><a href="#experience" className='text-lg p-2'>Experience</a></li>
-            <li><a href="/projects" className="text-lg p-2">Projects</a></li>
-            <li><a href="/contact" className="text-lg p-2">Contact</a></li>
+            <li><a href="#projects" className="text-lg p-2">Projects</a></li>
+            <li><a href="#contact" className="text-lg p-2">Contact</a></li>
           </div>
         </ul>
         </nav>
@@ -67,29 +68,49 @@ const Page: React.FC = () => {
               </span> {"in the summer of 2024."}
           </div>
 
-          {/* <div className='flex flex-col items-center mt-24 justify-start'>
-            <h1 className='text-4xl font-semibold mb-4 text-center'>{"Experience with the following technologies"}</h1>
-
-            <div className='skills flex space-justify-between'>
-              <LanguageCard language="Python" icon="/pythonlogo.png" />
-              <LanguageCard language="TypeScript" icon="/typescriptlogo.png" />
-              <LanguageCard language="Rust" icon="/rustlogo.png" />
-            </div>
-          </div> */}
-          <div></div>
-
-          <div id="experience" className='flex flex-col items-start pt-48 w-full'>
+          <div id="experience" className='flex flex-col items-start pt-36 w-full'>
             <h1 className='text-6xl font-semibold mb-6 text-center underline'>{"Experience"}</h1>
             <Job title="Software Engineering Intern · JP Morgan Chase & Co." date="JUNE 2024 - AUG 2024" description="I will be creating Full-Stack web applications that help to streamline the firm's business operations and gain experience creating quality software used for years to come." skills={['Typescript ', 'Database Management']} />
             <Job title="CS Department Teaching Assistant · The Ohio State University" date="AUG 2023 - DEC 2023" description="Graded homeworks, giving feedback and corrections to students, and held office hours for students taking Foundations of Computer Science I at The Ohio State University for the fall semester. Graded concepts like asymptotic complexity, discrete structures, and graph theory." skills={['Teaching', 'Communication', 'Computer Science Fundamentals', ]} />
             <Job title="AI and Web Developer Intern · Emerge Inc." date="MAY 2023 - AUG 2023" description="Learned how to create full-stack web applications from the frontend, creating user interfaces, to the backend creating routes in flask, creating data in databases and pulling data to display. Used ChatGPT to create innovative and inspiring applications that utilized the power of generative AI to speed up company's workflow" skills={['Flask', 'HTML', 'CSS', 'Prompt Engineering', 'Database Management', 'Hosting']} />
           </div>
 
+          <div id="projects">
+
+          </div>
+
+            <div className='flex flex-col items-start pt-36 w-full'>
+              <h1 className='text-6xl font-semibold mb-6 text-center underline'>{"Projects"}</h1>
+              <div className='flex flex-col space-y-4 items-center w-full'>
+                <Project 
+                  title="Rust File Scraper" 
+                  description="A command-line application developed in Rust that scrapes a file, finds instances of the input string, and returns it via the terminal." 
+                  technologies={['Rust']} 
+                  image="/rustlogo.png" 
+                  githubLink="https://github.com/DonovinNatividad/mini-grep" 
+                />
+                <Project 
+                  title="Personal Website" 
+                  description="A fun project I did to gain better experience in making frontend UIs and designing websites. Developed using Next.js, TypeScript, Tailwind CSS, and React." 
+                  technologies={['Next.js', 'TypeScript', 'Tailwind CSS', 'React']} 
+                  image="/portfolioimg.jpg" 
+                  githubLink="https://github.com/DonovinNatividad/personal-website" 
+                />
+                <Project 
+                  title="Personal Blog" 
+                  description="A beautiful static website where I post blogs about topics I find interesting. It's developed using Astro and React. The site is currently deployed, and I'm working on creating content for it." 
+                  technologies={['Astro', 'React']} 
+                  image="/blogimg.jpg" 
+                  githubLink="https://donovinnatividad.github.io/personal-blog/" 
+                />
+              </div>
+            </div>
+
 
         </div>
       </main>
-      <footer className='m-8'>
-      <a href="mailto:Natividad.10@osu.edu" className='contact text-center text-3xl p-3 hover:text-xl'>
+      <footer id="contact" className='mb-16'>
+      <a href="mailto:Natividad.10@osu.edu" className='contact text-center text-3xl p-4 hover:text-4xl'>
         Need to contact me?
       </a>
       </footer>
