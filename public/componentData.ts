93d0e49eae4d4e51bd85a3890de2048d5eac33ad
DonovinNatividad <Donovincall@gmail.com>
2024-12-08 21:53:39 -0500

    Finished refactoring project component to be more automated

--- public/componentData.ts
+++ public/componentData.ts
@@ -7,5 +7,23 @@ interface ProjectData {
 }
 
 export const projectData: ProjectData[] = [
-    { title: "Rust File Scraper", description: "A command-line application developed in Rust that scrapes a file, finds instances of the input string, and returns it via the terminal.", technologies: ['Rust'], image: "/rustlogo.png", githubLink: "https://github.com/DonovinNatividad/mini-grep" },
+    { 
+        title: "Rust File Scraper", description: "A command-line application developed in Rust that scrapes a file, finds instances of the input string, and returns it via the terminal.", technologies: ['Rust'], image: "/rustlogo.png", githubLink: "https://github.com/DonovinNatividad/mini-grep" 
+    },
+
+    {
+        title: "Personal Website", 
+        description: "A fun project I did to gain better experience in making frontend UIs and designing websites. Developed using Next.js, TypeScript, Tailwind CSS, and React.", 
+        technologies: ['Next.js', 'TypeScript', 'Tailwind CSS', 'React'], 
+        image: "/portfolioimg.JPG", 
+        githubLink: "https://github.com/DonovinNatividad/personal-website"
+    },
+
+    {
+        title: "Personal Blog", 
+        description: "A beautiful static website where I post blogs about topics I find interesting. It's developed using Astro and React. The site is currently deployed, and I'm working on creating content for it.", 
+        technologies:['Astro', 'React'],
+        image:"/blogimg.JPG", 
+        githubLink: "https://donovinnatividad.github.io/personal-blog/"
+    }
 ];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import LanguageCard from './components/LanguageCard';
 import Badge from './components/Badge';
 import Job from './components/Job';
 import Project from './components/Project';
+import { projectData } from '../../public/componentData';
 
 const Page: React.FC = () => {
   return (
@@ -83,27 +84,16 @@ const Page: React.FC = () => {
             <div className='flex flex-col items-start pt-36 w-full'>
               <h1 className='text-6xl font-semibold mb-6 text-center underline'>{"Projects"}</h1>
               <div className='flex flex-col space-y-4 items-center w-full'>
-                <Project 
-                  title="Rust File Scraper" 
-                  description="A command-line application developed in Rust that scrapes a file, finds instances of the input string, and returns it via the terminal." 
-                  technologies={['Rust']} 
-                  image="/rustlogo.png" 
-                  githubLink="https://github.com/DonovinNatividad/mini-grep" 
-                />
-                <Project 
-                  title="Personal Website" 
-                  description="A fun project I did to gain better experience in making frontend UIs and designing websites. Developed using Next.js, TypeScript, Tailwind CSS, and React." 
-                  technologies={['Next.js', 'TypeScript', 'Tailwind CSS', 'React']} 
-                  image="/portfolioimg.JPG" 
-                  githubLink="https://github.com/DonovinNatividad/personal-website" 
-                />
-                <Project 
-                  title="Personal Blog" 
-                  description="A beautiful static website where I post blogs about topics I find interesting. It's developed using Astro and React. The site is currently deployed, and I'm working on creating content for it." 
-                  technologies={['Astro', 'React']} 
-                  image="/blogimg.JPG" 
-                  githubLink="https://donovinnatividad.github.io/personal-blog/" 
-                />
+                {projectData.map((project) => (
+                  <Project 
+                    key={project.title}
+                    title={project.title}
+                    description={project.description}
+                    technologies={project.technologies}
+                    image={project.image}
+                    githubLink={project.githubLink}
+                  />
+                ))}
               </div>
             </div>
 
