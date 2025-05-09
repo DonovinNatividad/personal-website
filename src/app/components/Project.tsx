d1b6184cb2114c03a96ad5aa4e32e810dff9fd97
DonovinNatividad <Donovincall@gmail.com>
2025-05-08 22:24:41 -0400

    Made the Image Nextjs component smaller in my Project component as well as the large width of the Project component

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -11,9 +11,9 @@ interface ProjectProps {
 
 const Project: React.FC<ProjectProps> = ({ title, description, technologies, image, githubLink }) => {
     return (
-        <a href={githubLink} target="_blank" rel="noopener noreferrer" className=' lg:w-3/5 p-4 transform hover:scale-105 transition-transform duration-200'>
+        <a href={githubLink} target="_blank" rel="noopener noreferrer" className=' lg:w-1/3 p-4 transform hover:scale-105 transition-transform duration-200'>
             <div className='flex flex-col items-start bg-gray-900 rounded-xl p-4 h-full border-b border-black'>
-                <Image src={image} alt={title} className='w-full object-cover rounded-xl' height={378} width={512} />
+                <Image src={image} alt={title} className='w-full object-cover rounded-xl' height={256} width={384} />
                 <h2 className='mt-4 text-2xl font-bold text-rose-300'>{title}</h2>
                 <p className='mt-2 text-white flex-grow'>{description}</p>
                 <p className='mt-2 text-sm text-gray-300'>{technologies.join(', ')}</p>
